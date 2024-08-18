38b27994978aac8085cae7b4785806afd4932b28
Gresey Patidar <greseypatidar100@gmail.com>
2024-08-18 16:03:36 +0530

    Added sleedata object

--- backend/models/userData.js
+++ backend/models/userData.js
@@ -38,13 +38,13 @@ const userDataSchema = new mongoose.Schema({
         Saturday: { type: Number, default: 0 }
     },
     calorieBurnt: {
-        Sunday: { type: Number, default: 100 },
-        Monday: { type: Number, default: 400 },
-        Tuesday: { type: Number, default: 250 },
-        Wednesday: { type: Number, default: 500 },
-        Thursday: { type: Number, default: 200 },
+        Sunday: { type: Number, default: 0 },
+        Monday: { type: Number, default: 0 },
+        Tuesday: { type: Number, default: 0 },
+        Wednesday: { type: Number, default: 0},
+        Thursday: { type: Number, default: 0 },
         Friday: { type: Number, default: 0 },
-        Saturday: { type: Number, default: 200 }
+        Saturday: { type: Number, default: 0 }
     },
     calorieConsumed: {
         Sunday: { type: Number, default: 0 },
