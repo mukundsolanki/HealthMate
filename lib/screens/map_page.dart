32b16bc05b77bf6a4b62ec5f3254da8b8cc3b693
Mukund Solanki <mukundsolanki665@gmail.com>
2024-08-20 13:05:54 +0530

    User permissions

--- lib/screens/map_page.dart
+++ lib/screens/map_page.dart
@@ -1,66 +1,76 @@
-// @dart=2.17
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
-class MapPage extends StatefulWidget{
-  const MapPage({super.key});
+
+class MapPage extends StatefulWidget {
+  const MapPage({Key? key}) : super(key: key);
 
   @override
-  State<MapPage> createState() => _MapPageState();
+  _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> {
-
-  Location _locationController=new Location();
-  static const LatLng _pGooglePlex=LatLng(37.4220041,-122.0862462);
-  LatLng? _currectP=null;
+  late GoogleMapController mapController;
+  LatLng? _currentPosition;
+  Location location = Location();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    getLocationUpdates();
+    _getLocation();
   }
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      body: 
-      _currectP==null?
-        const Center(
-         child:Text("Loading..."),)  : GoogleMap(
-        initialCameraPosition:const CameraPosition(
-          target: _pGooglePlex,
-          zoom:13,),
-      markers:{const Marker(
-        markerId:MarkerId("_currentlocation"),
-        icon:BitmapDescriptor.defaultMarker,
-        position:_pGooglePlex)},),
-      
-    );
-  }
-  Future<void> getLocationUpdates() async{
+
+  Future<void> _getLocation() async {
     bool _serviceEnabled;
     PermissionStatus _permissionGranted;
-    _serviceEnabled=await _locationController.serviceEnabled();
-    if(_serviceEnabled){
-      _serviceEnabled=await _locationController.requestService();
-    }else{
-      
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
         return;
       }
-      _permissionGranted=await _locationController.hasPermission();
-      if(_permissionGranted==PermissionStatus.denied){
-        _permissionGranted=await _locationController.requestPermission();
-        if(_permissionGranted!=PermissionStatus.granted){
-         return;
-          }
-      }
-      _locationController.onLocationChanged.listen((LocationData currentLocation){
-       if(currentLocation.latitude!=null && currentLocation.longitude!=null){
-       setState(() {
-        _currectP=LatLng(currentLocation.latitude!, currentLocation.longitude!);  
-         print(_currectP);
-              });  }
-      });
-     
     }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    LocationData _locationData = await location.getLocation();
+
+    setState(() {
+      _currentPosition = LatLng(_locationData.latitude!, _locationData.longitude!);
+    });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Map Page"),
+      ),
+      body: _currentPosition == null
+          ? Center(child: CircularProgressIndicator())
+          : GoogleMap(
+              onMapCreated: (controller) {
+                mapController = controller;
+              },
+              initialCameraPosition: CameraPosition(
+                target: _currentPosition!,
+                zoom: 14.0,
+              ),
+              markers: {
+                Marker(
+                  markerId: MarkerId('currentLocation'),
+                  position: _currentPosition!,
+                  infoWindow: InfoWindow(title: 'Your Location'),
+                ),
+              },
+            ),
+    );
+  }
+}
